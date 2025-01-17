82d8fc20a9b5631dbafeed8055840c2fbe559c45
Wadecx <tyronmorcel@gmail.com>
2025-01-17 23:40:24 +0100

    Updates

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,16 @@
+import "./navbar.css"
+
 const Navbar = () => {
   return (
     <div className="navbar">
-        
+        <div className="leftSide">
+          <img src="https://assets.softr-files.com/applications/057d44a2-d021-4387-a972-0b37531cceee/assets/fb3fe59e-273e-4dd3-b468-1b16809dac25.png" alt="" />
+          <h1>MKBOX</h1>
+        </div>
+
+        <div className="rightSide">
+            <a href="https://www.marketingbox.fr/" target="_blank">Acceuil</a>
+        </div>
     </div>
   )
 }
